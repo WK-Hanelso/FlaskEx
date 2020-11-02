662073c198906cf562a01e39e675c5219b3bbacc
WK-Hanelso <WK.Hanelso@gmail.com>
2020-11-02 11:22:34 +0900

    add POST GET function
    POST 방식과 GET 방식을 구분하는 method 구현

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 ﻿import os           # 파일 핸들링
 from flask import Flask
+from flask import request
+from flask import redirect
 from flask import render_template
+
 from models import db
+from models import Fcuser
 
 app = Flask(__name__ )
 
-@app.route('/register')     # app의 main route가 '/'가 된다는 것이다.
+@app.route('/register', methods=['GET', 'POST'])     # app의 main route가 '/'가 된다는 것이다.
 def register():
+
+    if request.method=="POST":
+        userid = request.form.get("userid")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        re_password = request.form.get("re-password")
+
+        if( userid and username and password and re_password ) and ( password == re_password ):
+            fcuser = Fcuser()
+            fcuser.userid = userid
+            fcuser.username = username
+            fcuser.password = password
+            print( userid )
+            print( username)
+            print( password )
+            print( re_password)
+
+            db.session.add( fcuser )
+            db.session.commit()
+
+            return redirect('/')
+
     return render_template("register.html")
 
-@app.route('/')     # app의 main route가 '/'가 된다는 것이다.
+@app.route('/', methods = ["GET", "POST"])     # app의 main route가 '/'가 된다는 것이다.
 def hello():
     return render_template("hello.html")
 
@@ -17,11 +43,11 @@ if __name__=="__main__":
     print("app.py start!")
     # 현재 파일이 실행되는 경로
     basedir = os.path.abspath(os.path.dirname(__file__))        # 절대 경로 ( abspath )로 현재파일( __file__ )의 디렉토리 경로( dirname )를 이용하겠다.
-    print( "basedir:{}".format(basedir) )
+    # print( "basedir:{}".format(basedir) )
 
     # db파일이 저장되는 경로
     dbfile = os.path.join( basedir, "db.sqlite")                # basedir ( /home/hanelso/smb_dir/FlaskEx )에  db.sqlite 를 연결한다. -> /home/hanelso/smb_dir/FlaskEx/db.sqlite
-    print("file:{}".format(dbfile))
+    # print("file:{}".format(dbfile))
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
     app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True                  # 요청 받은 것에 대해서 응답을 할때를 Teardown 이라고 하는데 이때 commit을 하겠다는 의미
