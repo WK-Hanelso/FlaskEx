f1632e322640d4346f2a55c11200091b6b930fab
WK-Hanelso <WK.Hanelso@gmail.com>
2020-11-02 16:16:47 +0900

    add LED MODE
    led 모드를 추가하였다
    서버에서 LED를 킬수 있는 간단한 URL과 동작을 만들었다.

--- app.py
+++ app.py
@@ -7,7 +7,25 @@ from flask import render_template
 from models import db
 from models import Fcuser
 
+from ctypes import *
+
 app = Flask(__name__ )
+
+@app.route("/led/<color>/<mode>")
+def turnMode( color, mode ):
+
+    led = CDLL("./ledCont.so")
+
+    if color == "red":
+        pin = 1
+    elif color == "yellow":
+        pin = 4
+    elif color == "green":
+        pin = 5
+
+    led.ledControl( pin, mode )
+
+    return render_template("led.html", color = color, mode = mode)
 
 @app.route('/register', methods=['GET', 'POST'])     # app의 main route가 '/'가 된다는 것이다.
 def register():
@@ -43,11 +61,11 @@ if __name__=="__main__":
     print("app.py start!")
     # 현재 파일이 실행되는 경로
     basedir = os.path.abspath(os.path.dirname(__file__))        # 절대 경로 ( abspath )로 현재파일( __file__ )의 디렉토리 경로( dirname )를 이용하겠다.
-    # print( "basedir:{}".format(basedir) )
+    print( "basedir:{}".format(basedir) )
 
     # db파일이 저장되는 경로
     dbfile = os.path.join( basedir, "db.sqlite")                # basedir ( /home/hanelso/smb_dir/FlaskEx )에  db.sqlite 를 연결한다. -> /home/hanelso/smb_dir/FlaskEx/db.sqlite
-    # print("file:{}".format(dbfile))
+    print("file:{}".format(dbfile))
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
     app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True                  # 요청 받은 것에 대해서 응답을 할때를 Teardown 이라고 하는데 이때 commit을 하겠다는 의미
